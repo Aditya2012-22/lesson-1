db1ea0872b15a8e9d3e5c27e937399e5a3f21815
Aditya2012-22 <adi.adi22b@gmail.com>
2025-12-27 16:13:15 +0530

    initial commit

--- activity 1.py
+++ activity 1.py
@@ -5,5 +5,14 @@ print(1,2,3,4,5,6,7,sep="--")
 a=67
 print("a=",a)
 #this is a comment
+name_1 = "Rudra"
+name_2 = "Kritik"
+name_3 = "Kartik"
+name_4 = "Shobhiet"
+name_5 = "Aarav"
+print("my friends names are",name_1, name_2, name_3, name_4, name_5)
+
+
+
 import keyword
 print(keyword.kwlist)
