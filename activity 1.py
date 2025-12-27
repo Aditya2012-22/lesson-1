0f825e089d8d5abe392b6dbaa79e782383f8e8c4
Aditya2012-22 <adi.adi22b@gmail.com>
2025-12-27 16:05:26 +0530

    initial commit

---
+++ activity 1.py
@@ -0,0 +1,9 @@
+print("My name is Aditya",end="!!")
+print("I am 13yrs old")
+print("This a pthon class and I am doing lesson",1)
+print(1,2,3,4,5,6,7,sep="--")
+a=67
+print("a=",a)
+#this is a comment
+import keyword
+print(keyword.kwlist)
